56f5f587d59e1b8c7f1445f2f9b66e50204ba58c
OtakuBoy420 <U28282@rts-365.dk>
2023-02-13 10:51:19 +0100

    tailwind config done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,36 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors")
+const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F4A88E",
+        gray: "#E4E4E4",
+        white: colors.white,
+        black: colors.black,
+      },
+      fontFamily: {
+        sans: ["Arial", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        base: "22px",
+        lg: "28px",
+        xl: "50px",
+        "2xl": "62px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *")
+      addVariant("child-hover", "& > *:hover")
+    },
+    function ({ addBase }) {
+      addBase({
+        html: { fontSize: "22px" },
+      })
+    },
+  ],
 }
